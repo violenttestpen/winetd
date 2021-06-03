cd523ab3fce79b46a0e3b1680d78fa0064df8193
Winston Ho <winstonho90@users.noreply.github.com>
2021-06-03 15:48:05 +0800

    Use MAC integrity levels instead of token impersonation

--- impersonate.go
+++ impersonate.go
@@ -1,112 +1,54 @@
-// A demonstration example for http://stackoverflow.com/a/26124494
-// It runs a goroutine locked to an OS thread on Windows
-// then impersonates that thread as another user using its name
-// and plaintext password, then reverts to the default security
-// context before detaching from its OS thread.
 package main
 
 import (
 	"syscall"
 	"unsafe"
+
+	"github.com/violenttestpen/ginetd/internal/syscall/windows"
 )
 
-var (
-	advapi32        = syscall.NewLazyDLL("advapi32.dll")
-	logonProc       = advapi32.NewProc("LogonUserW")
-	impersonateProc = advapi32.NewProc("ImpersonateLoggedOnUser")
-	revertSelfProc  = advapi32.NewProc("RevertToSelf")
-)
-
-// func main() {
-// 	log.SetFlags(0)
-
-// 	var wg sync.WaitGroup
-// 	wg.Add(1)
-// 	go func(user, pass string) {
-// 		defer wg.Done()
-// 		runtime.LockOSThread()
-// 		defer runtime.UnlockOSThread()
-// 		log.Println("In a goroutine")
-// 		err := impersonate(user, pass)
-// 		if err != nil {
-// 			log.Fatal(err)
-// 		}
-// 		defer mustRevertToSelf()
-// 		// Here, we're impersonated as $user identified by $pass
-// 		log.Println("Impersonated")
-// 	}(user, pass)
-// 	wg.Wait()
-// 	log.Println("OK")
-// }
-
-func impersonate(user, pass string) error {
-	token, err := logonUser(user, pass)
-	if err != nil {
-		return err
-	}
-	defer mustCloseHandle(token)
-	return impersonateUser(token)
+var sidWinIntegrityLevels = map[string]string{
+	"Untrusted": `S-1-16-0`,
+	"Low":       `S-1-16-4096`,
 }
 
-func logonUser(user, pass string) (token syscall.Handle, err error) {
-	const (
-		// Taken from WinBase.h (SDK 7.1):
-		Logon32LogonNetwork    = uintptr(3)
-		Logon32ProviderDefault = uintptr(0)
-	)
-
-	// ".\0" meaning "this computer:
-	domain := [2]uint16{uint16('.'), 0}
-
-	var pu, pp []uint16
-	pu, err = syscall.UTF16FromString(user)
+func getIntegrityLevelToken(wns string) (syscall.Token, error) {
+	var procToken, token syscall.Token
+	proc, err := syscall.GetCurrentProcess()
 	if err != nil {
-		return
+		return 0, err
 	}
-	pp, err = syscall.UTF16FromString(pass)
+	defer syscall.CloseHandle(proc)
+
+	var access uint32 = syscall.TOKEN_DUPLICATE | syscall.TOKEN_ADJUST_DEFAULT |
+		syscall.TOKEN_QUERY | syscall.TOKEN_ASSIGN_PRIMARY
+	err = syscall.OpenProcessToken(proc, access, &procToken)
 	if err != nil {
-		return
+		return 0, err
 	}
+	defer procToken.Close()
 
-	rc, _, ec := syscall.Syscall6(logonProc.Addr(), 6,
-		uintptr(unsafe.Pointer(&pu[0])),
-		uintptr(unsafe.Pointer(&domain[0])),
-		uintptr(unsafe.Pointer(&pp[0])),
-		Logon32LogonNetwork,
-		Logon32ProviderDefault,
-		uintptr(unsafe.Pointer(&token)))
-	if rc == 0 {
-		err = error(ec)
-	}
-	return
-}
-
-func impersonateUser(token syscall.Handle) error {
-	rc, _, ec := syscall.Syscall(impersonateProc.Addr(), 1, uintptr(token), 0, 0)
-	if rc == 0 {
-		return error(ec)
-	}
-	return nil
-}
-
-func revertToSelf() error {
-	rc, _, ec := syscall.Syscall(revertSelfProc.Addr(), 0, 0, 0, 0)
-	if rc == 0 {
-		return error(ec)
-	}
-	return nil
-}
-
-func mustRevertToSelf() {
-	err := revertToSelf()
+	err = windows.DuplicateTokenEx(procToken, 0, nil, windows.SecurityImpersonation,
+		windows.TokenPrimary, &token)
 	if err != nil {
-		panic(err)
+		return 0, err
 	}
-}
 
-func mustCloseHandle(handle syscall.Handle) {
-	err := syscall.CloseHandle(handle)
+	sid, err := syscall.StringToSid(wns)
 	if err != nil {
-		panic(err)
+		return 0, err
 	}
+
+	tml := &windows.TOKEN_MANDATORY_LABEL{}
+	tml.Label.Attributes = windows.SE_GROUP_INTEGRITY
+	tml.Label.Sid = sid
+
+	err = windows.SetTokenInformation(token, syscall.TokenIntegrityLevel,
+		uintptr(unsafe.Pointer(tml)), tml.Size())
+	if err != nil {
+		token.Close()
+		return 0, err
+	}
+
+	return token, nil
 }
